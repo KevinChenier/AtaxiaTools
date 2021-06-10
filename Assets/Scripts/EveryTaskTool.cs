b7bf0f0b3f7708262e453ccee45a1ba562195284
EricFernandezPineda <bardock94@hotmail.com>
2021-06-09 23:16:48 -0400

    colliders added to scene + liquid spheres

---
+++ Assets/Scripts/EveryTaskTool.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EveryTaskTool : Tool<EverydayTaskConfig>
+{
+    public EveryTaskTool() : base("everydayTask") { }
+
+    public override int score()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void InitTool()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
