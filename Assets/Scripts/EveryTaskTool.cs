dc7ac82d19ba027fdbf2dd55a975949497dcbfc4
EricFernandezPineda <bardock94@hotmail.com>
2021-06-22 20:08:23 -0400

    update

--- Assets/Scripts/EveryTaskTool.cs
+++ Assets/Scripts/EveryTaskTool.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EveryTaskTool : Tool<EverydayTaskConfig>
 {
+    public int nbSpheres = 100;
+    public Text scoreText;
+
+    public GameObject container;
+    public float liquidSize;
+
+
     public EveryTaskTool() : base("everydayTask") { }
 
     public override int score()
@@ -19,7 +27,16 @@ public class EveryTaskTool : Tool<EverydayTaskConfig>
     // Start is called before the first frame update
     void Start()
     {
-        
+        var spawnPoint = container.transform.FindChildRecursive("SpawnLiquid");
+
+        for(int i = 0; i < nbSpheres; i++) 
+        {
+            
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z);
+            //GameObject sphere = Instantiate(magicPickup, new Vector3(transform.position.x, 0.5f, transform.position.z), Quaternion.identity) as GameObject;
+            sphere.transform.localScale = new Vector3(liquidSize, liquidSize, liquidSize);
+        }
     }
 
     // Update is called once per frame
